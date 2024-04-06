f98b2ee1040d68b01443e35fc320047fe2ed676e
Andrey Svyrskyi <akkad1v@gmail.com>
2024-04-06 20:29:30 +0300

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { NavLink } from "react-router-dom";
 import "./App.css";
+import css from "./App.module.css";
+import clsx from "clsx";
+
+const getNavLinkClassName = ({ isActive }) =>
+  clsx(css.navLink, { [css.active]: isActive });
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <header className={css.header}>
+        <nav className={css.nav}>
+          <NavLink className={getNavLinkClassName} to="/home">
+            Home
+          </NavLink>
+          <NavLink className={getNavLinkClassName} to="/movies">
+            Movies
+          </NavLink>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default App;
