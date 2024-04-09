3d8aa3b1ec7f46033169fa69de0ec61a730c2c64
Andrey Svyrskyi <akkad1v@gmail.com>
2024-04-09 20:27:40 +0300

    'update'

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
 import { getMovies } from "../../services/api";
 import { ErrorMessage } from "formik";
+import Loader from "../../components/Loader/Loader";
 import MoviesList from "../../components/MoviesList/MoviesList";
 
 const HomePage = () => {
   const [movies, setMovies] = useState(null);
   const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
+        setIsLoading(true);
         const response = await getMovies();
         console.log("response: ", response);
 
         setMovies(response.results);
       } catch (error) {
         setIsError(true);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchMovies();
@@ -24,6 +29,7 @@ const HomePage = () => {
   return (
     <div>
       <h1>Trending today</h1>
+      {isLoading && <Loader />}
       {movies && <MoviesList movies={movies} />}
       {isError && <ErrorMessage />}
     </div>
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -4,19 +4,24 @@ import { searchMovies } from "../../services/api";
 import { useEffect, useState } from "react";
 import { ErrorMessage } from "formik";
 import MoviesList from "../../components/MoviesList/MoviesList";
+import Loader from "../../components/Loader/Loader";
 
 const MoviesPage = () => {
   const [query, setQuery] = useState("");
   const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!query.length) return;
     const fetchMoviesByQuery = async () => {
       try {
+        setIsLoading(true);
         const data = await searchMovies(query);
         setQuery(data.results);
       } catch (error) {
         setIsError(true);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchMoviesByQuery();
@@ -45,6 +50,7 @@ const MoviesPage = () => {
         reverseOrder={false}
       />
       <SearchForm onSearchQuery={onSearchQuery} />
+      {isLoading && <Loader />}
       {isError && <ErrorMessage />}
       {query && <MoviesList movies={query} />}
     </div>
