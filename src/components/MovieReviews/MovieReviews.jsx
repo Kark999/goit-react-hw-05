47c386aa5456d96cfea0371f92c7f747f0641150
Andrey Svyrskyi <akkad1v@gmail.com>
2024-04-11 22:55:40 +0300

    'update'

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { requestMovieReview } from "../../services/api";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import css from "./MovieReviews.module.css";
+
 const MovieReviews = () => {
-  return <div>MovieReviews</div>;
+  const { movieId } = useParams();
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [movieReviews, setMovieReviews] = useState(null);
+
+  useEffect(() => {
+    if (!movieId) return;
+    async function fetchMovieIdReview() {
+      try {
+        setIsLoading(true);
+        const data = await requestMovieReview(movieId);
+        setMovieReviews(data.results);
+        console.log("data: ", data);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchMovieIdReview();
+  }, [movieId]);
+
+  return (
+    <div>
+      {isLoading && <Loader />}
+      <ul className={css.reviewList}>
+        {Array.isArray(movieReviews) &&
+          movieReviews.map((movieReview) => (
+            <li className={css.reviewListCard} key={movieReview.id}>
+              <p>
+                <b>Author</b>:{movieReview.author}
+              </p>
+              <p>{movieReview.content}</p>
+            </li>
+          ))}
+      </ul>
+      {isError && <ErrorMessage />}
+    </div>
+  );
 };
 
 export default MovieReviews;
